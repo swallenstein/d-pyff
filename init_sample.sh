22062a71ac36ad04a880f486cdfdf45ae19a09cd
r2h2 <rainer@hoerbe.at>
2016-04-05 17:45:59 +0200

    configuration only as example data

--- init_sample.sh
+++ init_sample.sh
@@ -4,4 +4,5 @@
 echo "initailizing /etc/pki /etc/pyff /var/md_source with sample data"
 cp -pr /opt/sample_data/etc/pki/* /etc/pki
 cp -pr /opt/sample_data/etc/pyff/* /etc/pyff
+cp -p /etc/pyff/md_aggregate_sign_swcert.fd-example/etc/pyff/md_aggregate_sign.fd
 cp -pr /opt/sample_data/var/md_source /var
