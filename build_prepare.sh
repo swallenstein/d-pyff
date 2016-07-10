da729da8aab8eb7e6bd418ff6b460f98b2208d00
r2h2 <rainer@hoerbe.at>
2016-07-10 22:13:26 +0200

    add xmlsectool

--- build_prepare.sh
+++ build_prepare.sh
@@ -6,7 +6,7 @@ workdir=$(dirname $BASH_SOURCE[0])
 cd $workdir
 
 get_or_update_repo() {
-    if [ -e $repodir ] ; then
+    if [ -e $repodir ]; then
         echo "pulling updates from $repodir"
         cd $repodir && git pull && cd $OLDPWD    # already cloned
     else
@@ -17,8 +17,18 @@ get_or_update_repo() {
 }
 
 
-# --- PVZDpolman ---
+# --- pyFF ---
 repodir='install/opt/pyff'
 repourl='https://github.com/rhoerbe/pyFF'
 get_or_update_repo
 
+# --- XMLSECTOOL ---
+repodir='xmlsectool-2'
+cd $workdir/install/opt/
+if [ ! -e $repodir ]; then
+    echo "downloading xmlsectool-2.0.0-beta-1-bin.zip"
+    wget https://shibboleth.net/downloads/tools/xmlsectool/2.0.0-beta-1/xmlsectool-2.0.0-beta-1-bin.zip
+    unzip xmlsectool-2.0.0-beta-1-bin.zip
+    ln -s xmlsectool-2.0.0-beta-1 $repodir
+    rm xmlsectool-2.0.0-beta-1-bin.zip
+fi
