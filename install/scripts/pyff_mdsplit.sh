c716bee99c2f2cf34f060696f11c5c612a3094cb
r2h2@kalypso <r2h2.kalypso@mail.hoerbe.at>
2016-07-12 22:12:01 +0200

    env passing

--- install/scripts/pyff_mdsplit.sh
+++ install/scripts/pyff_mdsplit.sh
@@ -7,15 +7,15 @@ if [ ! -e $MDSIGN_CERT ]; then echo "MDSIGN_CERT must be set and point to an exi
 if [ ! -e $MDSIGN_KEY ]; then echo "MDSIGN_KEY must be set and point to an existing file" && exit 1; fi
 if [ ! -e $MD_AGGREGATE ]; then echo "MD_AGGREGATE must be set and point to an existing file" && exit 1; fi
 # Setting defaults
-if [ ! -e $MDSPLIT_UNSIGNED ]; then MDSPLIT_UNSIGNED='/var/md_source/split/'; fi
-if [ ! -e $MDSPLIT_SIGNED ]; then MDSPLIT_SIGNED='/var/md_feed/split/'; fi
-if [ ! -e $LOGFILE ]; then LOGFILE='/var/log/pyffsplit.log'; fi
+if [ ! -z $MDSPLIT_UNSIGNED ]; then MDSPLIT_UNSIGNED='/var/md_source/split/'; fi
+if [ ! -z $MDSPLIT_SIGNED ]; then MDSPLIT_SIGNED='/var/md_feed/split/'; fi
+if [ ! -z $LOGFILE ]; then LOGFILE='/var/log/pyff_mdsplit.log'; fi
 
 
 # Step 1. Split aggregate and create an XML and a pipeline file per EntityDescriptor
-[ "$LOGLEVEL" == "DEBUG" ] && echo "processing "
+[ "$LOGLEVEL" == "DEBUG" ] && echo "processing md aggregate"
 /usr/bin/pyff_mdsplit.py \
-    --certfile $CERTFILE --keyfile $KEYFILE \
+    --certfile $MDSIGN_CERT --keyfile $MDSIGN_KEY \
     --outdir_signed pyff_split_sign_xmlsectool.sh \
     --logfile $LOGFILE --loglevel DEBUG \
     $MD_AGGREGATE $MDSPLIT_UNSIGNED
