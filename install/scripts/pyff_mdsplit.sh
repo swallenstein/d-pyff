5311477130ed6dab055b73c8865b60570dee38dd
r2h2@kalypso <r2h2.kalypso@mail.hoerbe.at>
2016-07-12 22:17:45 +0200

    env passing fix

--- install/scripts/pyff_mdsplit.sh
+++ install/scripts/pyff_mdsplit.sh
@@ -3,13 +3,13 @@ set -e
 # create a signed XML file per EntityDescriptor for ADFS
 
 # MDSIGN_CERT, MDSIGN_KEY and MDAGGREGATE must be passed via env
-if [ ! -e $MDSIGN_CERT ]; then echo "MDSIGN_CERT must be set and point to an existing file" && exit 1; fi
-if [ ! -e $MDSIGN_KEY ]; then echo "MDSIGN_KEY must be set and point to an existing file" && exit 1; fi
-if [ ! -e $MD_AGGREGATE ]; then echo "MD_AGGREGATE must be set and point to an existing file" && exit 1; fi
+if [ ! -e "$MDSIGN_CERT" ]; then echo "MDSIGN_CERT must be set and point to an existing file" && exit 1; fi
+if [ ! -e "$MDSIGN_KEY" ]; then echo "MDSIGN_KEY must be set and point to an existing file" && exit 1; fi
+if [ ! -e "$MD_AGGREGATE" ]; then echo "MD_AGGREGATE must be set and point to an existing file" && exit 1; fi
 # Setting defaults
-if [ ! -z $MDSPLIT_UNSIGNED ]; then MDSPLIT_UNSIGNED='/var/md_source/split/'; fi
-if [ ! -z $MDSPLIT_SIGNED ]; then MDSPLIT_SIGNED='/var/md_feed/split/'; fi
-if [ ! -z $LOGFILE ]; then LOGFILE='/var/log/pyff_mdsplit.log'; fi
+if [ ! -z "$MDSPLIT_UNSIGNED" ]; then MDSPLIT_UNSIGNED='/var/md_source/split/'; fi
+if [ ! -z "$MDSPLIT_SIGNED" ]; then MDSPLIT_SIGNED='/var/md_feed/split/'; fi
+if [ ! -z "$LOGFILE" ]; then LOGFILE='/var/log/pyff_mdsplit.log'; fi
 
 
 # Step 1. Split aggregate and create an XML and a pipeline file per EntityDescriptor
