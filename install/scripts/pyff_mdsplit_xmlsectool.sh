507fbacc89ae5b4bc0423b4af4b8b32afda21969
r2h2 <rainer@hoerbe.at>
2016-09-14 12:02:22 +0200

    fixes

--- install/scripts/pyff_mdsplit_xmlsectool.sh
+++ install/scripts/pyff_mdsplit_xmlsectool.sh
@@ -3,34 +3,28 @@ set -e
 # create a signed XML file per EntityDescriptor for ADFS
 
 # MDSIGN_CERT, MDSIGN_KEY and MDAGGREGATE must be passed via env
-if [ ! -e $MDSIGN_CERT ]; then echo "MDSIGN_CERT must be set and point to an existing file" && exit 1; fi
-if [ ! -e $MDSIGN_KEY ]; then echo "MDSIGN_KEY must be set and point to an existing file" && exit 1; fi
-if [ ! -e $MD_AGGREGATE ]|| echo "MD_AGGREGATE must be set and point to an existing file" && exit 1; fi
+if [ ! -e "$MDSIGN_CERT" ]; then echo "MDSIGN_CERT must be set and point to an existing file" && exit 1; fi
+if [ ! -e "$MDSIGN_KEY" ]; then echo "MDSIGN_KEY must be set and point to an existing file" && exit 1; fi
+if [ ! -e "$MD_AGGREGATE" ]; then echo "MD_AGGREGATE must be set and point to an existing file" && exit 1; fi
 # Setting defaults
-if [ ! -e $MDSPLIT_UNSIGNED ]; then MDSPLIT_UNSIGNED='/var/md_source/split/'; fi
-if [ ! -e $MDSPLIT_SIGNED ]; then MDSPLIT_SIGNED='/var/md_feed/split/'; fi
-if [ ! -e $LOGFILE ]; then LOGFILE='/var/log/pyffsplit.log'; fi
+if [ -z "$MDSPLIT_UNSIGNED" ]; then MDSPLIT_UNSIGNED='/var/md_source/split/'; fi
+if [ -z "$MDSPLIT_SIGNED" ]; then MDSPLIT_SIGNED='/var/md_feed/split/'; fi
+if [ -z "$LOGFILE" ]; then LOGFILE='/var/log/pyff_mdsplit.log'; fi
+
 
 
 # Step 1. Split aggregate and create an XML and a pipeline file per EntityDescriptor
-[ "$LOGLEVEL" == "DEBUG" ] && echo "processing "
-/usr/bin/mdsplit.py \
-    -c $CERTFILE -k $KEYFILE \
-    -l $LOGFILE -L DEBUG \
-    $MD_AGGREGATE $MDSPLIT_UNSIGNED $MDSPLIT_SIGNED
+[ "$LOGLEVEL" == "DEBUG" ] && echo "processing md aggregate"
+/usr/bin/pyff_mdsplit.py $* \
+    --nocleanup \
+    --nosign \
+    --logfile $LOGFILE --loglevel DEBUG \
+    $MD_AGGREGATE $MDSPLIT_UNSIGNED
 
-
-# Step 2. Execute pyff to sign each EntityDescriptor
-#cd /var/md_source/split/
-#for fn in *.fd; do
-#    echo "running pyff for $fn"
-#    /usr/bin/pyff --loglevel=$LOGLEVEL $fn
-#done
 
 # Step 2. Execute xmlsectool to sign each EntityDescriptor (ignoring pyff pipeline)
 # Problem: pyff does not create signatures with exclusive c14n (http://www.w3.org/2001/10/xml-exc-c14n#)
 # -> use xmlsectool for ADFS
-
 cd $MDSPLIT_UNSIGNED
 [ "$LOGLEVEL" == "DEBUG" ] && VERBOSE='--verbose'
 mkdir -p $MDSPLIT_SIGNED
