af497a72994b242bb4186a4a9552e1177ff90986
r2h2 <rainer@hoerbe.at>
2017-04-30 20:54:31 +0200

    minor fixes

--- install/scripts/status.sh
+++ install/scripts/status.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 echo
-netstat -tunap | head -2
-netstat -tunap | egrep ':8080\s'
+lsof -i -P -sTCP:LISTEN
 echo
 ps -eaf | head -1
 ps -eaf | grep ' /usr/bin/pyffd ' | grep -v ' grep '
