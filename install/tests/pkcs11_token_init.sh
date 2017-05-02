e77e024303dd7a87970f46355448ff5ab51298e4
r2h2 <rainer@hoerbe.at>
2017-05-02 15:32:21 +0200

    refactor: remove hard-coded values

---
+++ install/tests/pkcs11_token_init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+
+[ -z "$TOKENPW" ] && TOKENPW='secret1'
+[ -z "$SecurityOfficerPIN" ] && SecurityOfficerPIN='secret2'
+echo 'Initializing Token'
+pkcs11-tool --module $PKCS11_CARD_DRIVER --init-token --label test --pin $TOKENPW --so-pin $SecurityOfficerPIN || exit -1
+
+echo 'Initializing User PIN'
+pkcs11-tool --module $PKCS11_CARD_DRIVER --login --init-pin --pin $TOKENPW --so-pin $SecurityOfficerPIN
+
+echo 'Generating RSA key'
+pkcs11-tool --module $PKCS11_CARD_DRIVER --login --keypairgen --key-type rsa:2048 -d 1 --label test --pin $TOKENPW || exit -1
+
+echo 'Checking objects on card'
+pkcs11-tool --module $PKCS11_CARD_DRIVER --login -O --pin $TOKENPW || exit -1
