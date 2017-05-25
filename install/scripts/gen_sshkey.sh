708aa8a0a0b9d4da7a2f582a63cc929ba185171d
r2h2 <rainer@hoerbe.at>
2017-05-25 17:11:17 +0200

    do not check image signature in CI

--- install/scripts/gen_sshkey.sh
+++ install/scripts/gen_sshkey.sh
@@ -4,7 +4,7 @@ if (( $(id -u) == 0 )); then
     echo "Do not start as root: ssh keys are required for the regular container user."
 fi
 
-if [[ ! -e ~/.ssh/id_ecdsa ]]; then
+if [[ ! -e ~/.ssh/id_ed25519 ]]; then
     ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ''
 fi
 
@@ -12,4 +12,4 @@ fi
 ssh -T $MDFEED_SSHUSER@$MDFEED_HOST
 
 echo "created new public key - register with $MDFEED_HOST/$MDFEED_REPO:"
-cat ~/.ssh/id_ed25519.pub
+cat ~/.ssh/id_ed25519.pub || true
