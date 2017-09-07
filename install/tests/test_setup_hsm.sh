d5c961452ec6007ff098ca22dfbbbca7538b7e7f
r2h2 <rainer@hoerbe.at>
2017-09-08 01:34:17 +0200

    fix test scripts

--- install/tests/test_setup_hsm.sh
+++ install/tests/test_setup_hsm.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 echo 'Test setup HSM: set test config and add mdsource data (not overwriting existing data)'
-cp /opt/testdata/etc/pyff/md_aggregator_sign_hsm.fd-example /etc/pyff/md_aggregator_sign_hsm.fd
 export PIPELINEBATCH=/etc/pyff/md_aggregator_sign_hsm.fd
 
