4427281a451eba029f97c857bfe5fe263a0e24f3
r2h2 <rainer@hoerbe.at>
2017-09-07 20:02:17 +0200

    fix test scripts

--- install/tests/test_pyff.sh
+++ install/tests/test_pyff.sh
@@ -9,7 +9,7 @@ set +e
 
 
 # setup test configuration
-cp -pr /opt/etc/pyff/* etc/pyff/
+cp -pr /opt/testdata/etc/pyff/* /etc/pyff/
 
 # test 21
 echo "Test 21: create aggregate from test data. Pipeline: ${PIPELINEBATCH}"
