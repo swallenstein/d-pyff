f8d8de18726c20337735258b3045ce8108f6509c
r2h2 <rainer@hoerbe.at>
2017-10-06 12:36:18 +0200

    add option to start pyffd on non-standard port (for docker net=host)

--- install/scripts/start_pyffd.sh
+++ install/scripts/start_pyffd.sh
@@ -5,12 +5,14 @@ if (( $(id -u) == 0 )); then
     echo "Do not start as root: ssh keys are required for the regular container user."
 fi
 
+[[ $PORT ]] || PORT='8080'
 
 /usr/bin/pyffd -a -f --proxy \
     --log=$LOGDIR/pyffd.log \
     --error-log=$LOGDIR/pyffd.error \
     --loglevel=$LOGLEVEL \
     --frequency=$FREQUENCY \
-    -H0.0.0.0 \
+    --port=$PORT \
+    --host=0.0.0.0 \
     -p $PIDFILE \
     $PIPELINEDAEMON
